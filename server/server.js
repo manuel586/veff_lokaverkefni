f133ece7c976aba4f0dccc66cec01e04ab366aba
Manuel Björn Gomes Jóhannsson <FS403719@fss.is>
2016-11-16 10:08:51 +0000

    removed comment functionality

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ var config = require('../webpack.config.js');
 var webpack = require('webpack');
 var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpackHotMiddleware = require('webpack-hot-middleware');
-var fs = require('fs');
 var bodyParser = require('body-parser');
 
 
@@ -20,47 +19,6 @@ app.use(express.static('./dist'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-
-
-var COMMENTS_FILE = path.join(__dirname, './comments.json');
-
-app.get('/api/comments', function(req, res) {
-	debugger
-  fs.readFile(COMMENTS_FILE, function(err, data) {
-  	console.log(COMMENTS_FILE);
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    res.json(JSON.parse(data));
-  });
-});
-
-app.post('/api/comments', function(req, res) {
-  fs.readFile(COMMENTS_FILE, function(err, data) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    var comments = JSON.parse(data);
-    // NOTE: In a real implementation, we would likely rely on a database or
-    // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
-    // treat Date.now() as unique-enough for our purposes.
-    var newComment = {
-      id: Date.now(),
-      author: req.body.author,
-      text: req.body.text,
-    };
-    comments.push(newComment);
-    fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function(err) {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
-      res.json(comments);
-    });
-  });
-});
 
 app.use('/', function(req,res){
   res.sendFile(path.resolve('client/index.html'));
