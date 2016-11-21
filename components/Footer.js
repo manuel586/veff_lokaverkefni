f84cf7944089612863404bd458fe067b53594b7f
Manuel Björn Gomes Jóhannsson <FS403719@fss.is>
2016-11-21 09:16:06 +0000

    Búin að breyta footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,11 +3,14 @@ import React from 'react'
 var Footer = React.createClass({
 		render: function(){
 			var footerStyle = {
-				border: 'solid green'
+				border: 'solid #006400',
+				background: '#556B2F'
 			};
 				return(
 						<div style={footerStyle}>
-							<h2>Þetta er footer</h2>
+							<h2>Manuel Björn Gomes Jóhannsson</h2>
+							<h2>VFOR2AN05</h2>
+							<h2><a href="https://github.com/manuel586">Github</a></h2>
 						</div>
 					);
 		}
